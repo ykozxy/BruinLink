95a95c5598966fb880701427fdd5de3b45df4ed2
ykozxy <nick_zhouxy@outlook.com>
2021-11-03 01:14:15 -0700

    Created react-routers in index.js
    Added routers to index.js, which will serve as the control center for future page redirects and navigations. Currently, only routers for the register and login page is set up. Any request to "/" will be redirected to the login page.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Redirect, Route} from "react-router-dom";
 import CssBaseline from '@mui/material/CssBaseline';
 import {ThemeProvider} from '@mui/material/styles';
 import theme from './theme';
+import LoginPage from "./components/loginPage"
+import RegisterPage from "./components/registerPage";
+
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
         <CssBaseline/>
+        {/* Setup react routes for page redirection */}
+        <BrowserRouter>
+            <Route path="/">
+                {/* TODO: put main page here and replace the Redirect element */}
+                <Redirect to="/login"/>
+            </Route>
+            <Route path="/register" component={RegisterPage}/>
+            <Route path="/login" component={LoginPage}/>
+        </BrowserRouter>
     </ThemeProvider>,
     document.querySelector('#root'),
 );
