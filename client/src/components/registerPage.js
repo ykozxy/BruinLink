dc6755fb386950b7074b961eea4a371afd455288
ykozxy <nick_zhouxy@outlook.com>
2021-11-03 01:11:45 -0700

    Created registerPage.js
    Created the file registerPage.js, which will eventually display the register account webpage.

---
+++ client/src/components/registerPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Box} from "@mui/material";
+
+
+export default class RegisterPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <Box>
+                    Register Page
+                </Box>
+            </div>
+        );
+    }
+}
