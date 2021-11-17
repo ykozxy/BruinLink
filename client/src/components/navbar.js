0528f9ef5cca1ad94cee8d79113e4fa54ba3bf55
ykozxy <nick_zhouxy@outlook.com>
2021-11-17 13:21:47 -0800

    Organized React router setup
    Moved codes from clientRouters.js to index.js, and fixed Redirect link in navbar.js.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,9 +1,7 @@
-import React,{useState} from 'react'
+import React from 'react'
 import {Link} from "react-router-dom";
 import "./navbar.css";
 import SearchIcon from '@mui/icons-material/Search';
-
-
 
 
 function Navbar() {
@@ -11,14 +9,14 @@ function Navbar() {
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="./homepage" className="navbar-logo">
-                        <text className="BL-logo"> BruinLink </text>
+                    <Link to="/" className="navbar-logo">
+                        <text className="BL-logo"> BruinLink</text>
                     </Link>
 
                     <ul className="navbar-menu">
                         <li className="navbar-item">
                             <Link to="/search" className="navbar-links">
-                                Search   <SearchIcon />
+                                Search <SearchIcon/>
                             </Link>
                         </li>
                         <li className="navbar-item">
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Switch, Redirect, Route} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import CssBaseline from '@mui/material/CssBaseline';
 import {ThemeProvider} from '@mui/material/styles';
 import theme from './theme';
 import LoginPage from "./components/loginPage"
 import RegisterPage from "./components/registerPage";
 import Navbar from "./components/navbar";
-import ClientRouters from "./clientRouters";
+import Test from "./testing/testPage";
 
 
 ReactDOM.render(
@@ -15,18 +15,17 @@ ReactDOM.render(
         <CssBaseline/>
         {/* Setup react routes for page redirection */}
         <BrowserRouter>
-            <Navbar />
-                <Switch>
-                    <Route path="/" exact />
-                </Switch>
-            <Route path="/">
-                {/* TODO: put main page here and replace the Redirect element */}
-                <Redirect to="/login"/>
-            </Route>
-            <Route path="/register" component={RegisterPage}/>
-            <Route path="/login" component={LoginPage}/>
+            <Navbar/>
+            <Switch>
+                <Route path="/">
+                    {/* TODO: put main page here and replace the Redirect element */}
+                    <Redirect to="/login"/>
+                </Route>
+                <Route path="/register" component={RegisterPage}/>
+                <Route path="/login" component={LoginPage}/>
+                <Route path="/test" component={Test}/>
+            </Switch>
         </BrowserRouter>
-        <ClientRouters/>
     </ThemeProvider>,
     document.querySelector('#root'),
 );
