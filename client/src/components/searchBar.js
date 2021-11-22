b89ee9c70ae9f53949b5ebed2e8bcfad82b364e2
Lucky B <fushiyan@g.ucla.edu>
2021-11-21 17:05:01 -0800

    Create searchBar.js

---
+++ client/src/components/searchBar.js
@@ -0,0 +1,39 @@
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import "./navbar.css";
+import React, { Component } from 'react'
+
+
+export default class searchBar extends Component {
+    render() {
+        return(
+            <Search>
+                <SearchIconWrapper>
+                    <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                    placeholder="Search…"
+                    inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+        );
+    }
+}
+
+
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
+
+  
