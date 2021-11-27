7be666aa07d167d9eb2978a005c6b03c3ea1bcdc
Nvgui <dailaiyin@g.ucla.edu>
2021-11-27 00:21:34 -0800

    modify avatar display

--- client/src/components/letterAvatar.js
+++ client/src/components/letterAvatar.js
@@ -4,18 +4,19 @@ import Stack from '@mui/material/Stack';
 import { deepOrange, deepPurple } from '@mui/material/colors';
 import { render } from 'react-dom';
 import Cookies from 'js-cookie';
+import bruin from '../images/bruin.jpg'
 
 class LetterAvatars extends React.Component{
     render(){
         {
-            let avatar=Cookies.get("accountID");
+            //let avatar=Cookies.get("email");
             //let avatar="ABC"
         
         return (
             <Stack direction="row" spacing={2}>
               {/* <Avatar>A</Avatar>
               <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar> */}
-              <Avatar sx={{ bgcolor: deepPurple[500] }}>{avatar}</Avatar>
+              <Avatar alt="Bruin" src={bruin} />
             </Stack>
           )
         }
