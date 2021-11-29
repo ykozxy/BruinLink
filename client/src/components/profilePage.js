70c78d42e968038473d0d33308f468ad96f3e0c6
Lucky B <fushiyan@g.ucla.edu>
2021-11-28 16:16:49 -0800

    profile update

--- client/src/components/profilePage.js
+++ client/src/components/profilePage.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import Navbar from "./navbar";
+import Cookies from 'js-cookie';
 
-export default function ProfilePage() {
-    return (
-        <div>
-            <Navbar isLogin={true} />
-            <h1 className='profilePage'>This is your profile</h1>
-        </div>
-    )
+class ProfilePage extends React.Component{
+    render(){
+        {
+            let c = Cookies.get("accountID");
+            //let c = "123";
+                  if (!c) {
+                    return (
+                        <div>
+                            <Navbar isLogin={false} />
+                            <h1 className='profilePage'>Please Log in to view your profile</h1>
+                        </div>
+                    )
+                  }
+        }
+        return (
+            <div>
+                <Navbar isLogin={true} />
+                <h1 className='profilePage'>This is your profile</h1>
+            </div>
+        )
+    }
+
 }
+
+export default ProfilePage
